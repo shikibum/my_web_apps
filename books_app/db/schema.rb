561f98cf9469c16b1444e4fd1e89f5c7720c2f13
shikibum <satoshikibu@gmail.com>
2018-08-17 17:29:59 +0900

    add picture

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_17_065954) do
+ActiveRecord::Schema.define(version: 2018_08_17_081727) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_08_17_065954) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
