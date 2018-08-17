b792e5bc8fdcf4024d481c75f3640135b7bc579a
shikibum <satoshikibu@gmail.com>
2018-08-17 16:09:15 +0900

    add_author

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_094821) do
+ActiveRecord::Schema.define(version: 2018_08_17_065954) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
