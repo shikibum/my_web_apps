c17e541ce0eac594c71fc924b1dc6f49528b55ee
shikibum <satoshikibu@gmail.com>
2018-08-21 10:38:58 +0900

    rubocop

--- books_app/app/controllers/books_controller.rb
+++ books_app/app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books
   # GET /books.json
@@ -14,8 +16,7 @@ class BooksController < ApplicationController
 
   # GET /books/1
   # GET /books/1.json
-  def show
-  end
+  def show; end
 
   # GET /books/new
   def new
@@ -25,8 +26,7 @@ class BooksController < ApplicationController
   end
 
   # GET /books/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /books
   # POST /books.json
@@ -69,13 +69,14 @@ class BooksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def book_params
-      params.require(:book).permit(:title, :memo, :author, :picture)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def book_params
+    params.require(:book).permit(:title, :memo, :author, :picture)
+  end
 end
--- books_app/config/routes.rb
+++ books_app/config/routes.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'books/index'
   resources :books
 
-  scope "/:locale" do
+  scope '/:locale' do
     resources :books
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
