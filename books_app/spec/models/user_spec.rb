e74263e8a7955c265bb36712e16ec90089509946
shikibum <satoshikibu@gmail.com>
2018-09-17 23:03:58 +0900

    update_with_passwordメソッドのテスト

--- books_app/spec/models/user_spec.rb
+++ books_app/spec/models/user_spec.rb
@@ -16,4 +16,27 @@ RSpec.describe User do
       end
     end 
   end
+
+  describe 'update_with_password' do
+    context 'Twitter_userの場合' do
+      it 'パスワードなしで更新できる' do
+        user = User.create(email: 'hoge@hoge.com', provider: 'twitter', uid: '1013638572186800129')
+        expect(user.update_with_password(name: 'shikibu')).to eq true
+        user.reload
+        expect(user.name).to eq 'shikibu'
+      end
+    end
+
+    context 'Twitter_userでない場合' do
+      it 'パスワードなしだと更新できない' do
+        user = User.create(email: 'hoge@hoge.com', password: 'passpass')
+        expect(user.update_with_password(name: 'shikibu')).to eq false
+      end
+
+      it 'パスワードありだと更新できる' do
+        user = User.create(email: 'hoge@hoge.com', password: 'passpass')
+        expect(user.update_with_password(name: 'shikibu', current_password: 'passpass')).to eq true
+      end
+    end
+  end
 end
