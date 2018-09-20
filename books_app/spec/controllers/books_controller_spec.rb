4b30cfbbd89a0c533d90191f095b799827f76983
shikibum <satoshikibu@gmail.com>
2018-09-21 00:33:40 +0900

    stubの書き方修正

--- books_app/spec/controllers/books_controller_spec.rb
+++ books_app/spec/controllers/books_controller_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
+  render_views
   describe '#index' do
     context '@booksが返ってくる' do
-      let(:book) { Book.create(title: 'Don quijote', memo: 'ドン・キホーテ', author: 'Cervantes') }
+      let!(:book) { Book.create(title: 'Don quijote', memo: 'ドン・キホーテ', author: 'Cervantes') }
       let(:user) { User.create(email: 'hoge@hoge.com', password: 'passpass') }
       it '' do
         login_user(user)
         get :index
-        binding.pry
-        # expect(response).to 
+        expect(response.body).to include('Don quijote')
       end
     end
   end
