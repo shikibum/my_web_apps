7a9d718278aa9593b4820c134010d032dd3c5f37
shikibum <satoshikibu@gmail.com>
2018-09-25 23:16:55 +0900

    view spec

---
+++ books_app/spec/views/books/show.html.erb_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "books/show" do
+    before(:each) do
+      @book = Book.create(title: 'Don quijote', memo: 'ドン・キホーテ', author: 'Cervantes')
+    end
+
+    it 'redering with the show template' do
+      assign(:book, @book)
+      render template: 'books/show.html.erb'
+      expect(rendered).to match /Don quijote/
+    end
+end
