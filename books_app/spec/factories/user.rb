47987d82db57535084e60ac605303c56703033e9
shikibum <satoshikibu@gmail.com>
2018-10-02 23:36:24 +0900

    factorybot、spring-commands-rspecを追加

---
+++ books_app/spec/factories/user.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email { "hoge@hoge.com" }
+    password { "passpass" }
+  end
+end
--- books_app/spec/rails_helper.rb
+++ books_app/spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/rspec'
 require 'devise'
 require File.expand_path("spec/support/controller_macros.rb")
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -22,7 +23,7 @@ require File.expand_path("spec/support/controller_macros.rb")
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -64,6 +65,7 @@ RSpec.configure do |config|
   # devise
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerMacros, type: :controller
-
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
 end
 
