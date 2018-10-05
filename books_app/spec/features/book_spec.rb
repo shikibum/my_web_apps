dbfb00f3ea21864a5b3110155c8f4a7c6558d802
shikibum <satoshikibu@gmail.com>
2018-10-05 21:00:38 +0900

    feature specにuser_spec、book_specを追加

---
+++ books_app/spec/features/book_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'selenium-webdriver'
+
+RSpec.feature 'Books', type: :feature do
+  let(:user){ FactoryBot.create(:user) }
+
+  scenario 'user creates a new book, show index, show, edit and destroy the book', js: true do
+    visit root_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+
+    expect{
+      click_link '本を追加'
+      fill_in 'タイトル',	with: 'Test Book'
+      fill_in 'メモ',	with: 'Test Memo'
+      fill_in '著者',	with: 'Test Author'
+      click_button '登録する'
+
+      expect(page).to have_content 'Book was successfully created'
+      expect(page).to have_content 'Test Book'
+    }.to change(Book, :count).by(1)
+
+    visit root_path
+    expect(page).to have_content 'Test Book'
+
+    click_link 'Show'
+    click_link 'Edit'
+    fill_in 'タイトル',	with: 'てすとぶっく'
+    click_button '更新する'
+    expect(page).to have_content 'てすとぶっく'
+    click_link 'Back'
+
+    expect{
+    click_link 'Destroy'
+    driver = Selenium::WebDriver.for :chrome
+    accept_alert do
+      alert = driver.switch_to.alert.accept rescue Selenium::WebDriver::Error::NoAlertOpenError
+    end
+    }.to change(Book, :count).by(0)
+
+  end
+end
--- books_app/spec/features/user_spec.rb
+++ books_app/spec/features/user_spec.rb
@@ -1,22 +1,39 @@
 # # frozen_string_literal: true
 
 require 'rails_helper'
+require 'selenium-webdriver'
 
 RSpec.feature 'Users', type: :feature do
-  scenario 'sign up new user' do
+  scenario 'sign up new user, edit, logout, login, and destroy', js: true do
     visit new_user_registration_path
-    # visit root_path
-    # within '.navbar-link' do
-    #   click_link 'Sign up'
-    # end
-  
     expect{
       fill_in 'Email', with: 'hoge@hoge.com'
       fill_in 'Password', with: 'passpass'
       fill_in 'Password confirmation', with: 'passpass'
       click_button 'Sign up'
-save_and_open_page
     }.to change(User, :count).by(1)
+
+    click_link 'Logout'
+
+    fill_in 'Email', with: 'hoge@hoge.com'
+    fill_in 'Password', with: 'passpass'
+    click_button 'Log in'
+
+    click_link 'Edit profile'
+    fill_in 'Email', with: 'kawauso@hoge.com'
+    fill_in 'Current password', with: 'passpass'
+    click_button 'Update'
+    expect(page).to have_content 'Logged in as kawauso'
+
+    expect{  
+      click_link 'Edit profile'
+      click_button 'Cancel my account'
+      driver = Selenium::WebDriver.for :chrome
+      accept_alert do
+        alert = driver.switch_to.alert.accept rescue Selenium::WebDriver::Error::NoAlertOpenError
+      end
+    }.to change(User, :count).by(0)
+    expect(page).to have_content 'Log in'
   end
 
   scenario 'sign up new user with Twitter' do
